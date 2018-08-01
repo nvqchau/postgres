2e09c6a54f1cb9f92b67af3aa68afa5c94c2e848
Chau Nguyen <nvqchau@hotmail.com>
2018-08-01 18:06:54 +0700

    Try to add version to postgresql-contrib

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,7 @@ FROM postgres:9.6.1
 LABEL version="1.0"
 LABEL maintainer="nvqchau@hotmail.com"
 
+RUN apt-get update && apt-get install postgresql-contrib-9.4
+
 ADD createExtension.sh /docker-entrypoint-initdb.d/
 RUN chmod 755 /docker-entrypoint-initdb.d/createExtension.sh
--- createExtension.sh
+++ createExtension.sh
@@ -3,19 +3,6 @@
 
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$POSTGRES_DB" <<-EOSQL
-create schema extensions;
-
--- make sure everybody can use everything in the extensions schema
-grant usage on schema extensions to public;
-grant execute on all functions in schema extensions to public;
-
--- include future extensions
-alter default privileges in schema extensions
-   grant execute on functions to public;
-
-alter default privileges in schema extensions
-   grant usage on types to public;
-
-create extension if not exists "uuid-ossp" schema extensions;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+   CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 EOSQL
